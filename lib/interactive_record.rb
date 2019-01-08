d2270e7028e3fa579ea2da7ffe7b5eec7fc7cfe4
Joe Ganobsik <jganobsik@gmail.com>
2019-01-08 21:22:09 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -22,6 +22,18 @@ end
   persisted? ? update : insert
   end
   
+   def self.column_names
+
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
+  
 def persisted?
   !!self.id 
 end
