20ca85ec00b86aa82a4a90a5fd72a2528ae52f17
Joe Ganobsik <jganobsik@gmail.com>
2019-01-08 20:46:05 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -18,6 +18,18 @@ class InteractiveRecord
   def self.attribute_values
 end
 
-self.save 
+def save
+   def save 
+  
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+ self
+  end
+end
 
 end
