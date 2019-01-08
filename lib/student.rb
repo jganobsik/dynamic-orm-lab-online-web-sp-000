c1793795d6e5889e178caa98f4656ae59186fa3d
Joe Ganobsik <jganobsik@gmail.com>
2019-01-08 21:24:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,9 +4,8 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
   
-  attr_accessor :name, :grade
   
-  def initialize(attributes)
-    
+ self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
   end
 end
